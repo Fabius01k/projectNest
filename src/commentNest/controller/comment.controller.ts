020314085d4f758d217a55e4764e6bce87dc086e
Fabius01k <pashamuruskinn@gmail.com>
2023-08-30 16:09:03 +0300

    versel.json

--- src/commentNest/controller/comment.controller.ts
+++ src/commentNest/controller/comment.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Param, Res } from '@nestjs/common';
 import { CommentView } from '../schema/comment.schema';
 import { CommentService } from '../service/comment.service';
 import { Response } from 'express';
+
 @Controller('comments')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
