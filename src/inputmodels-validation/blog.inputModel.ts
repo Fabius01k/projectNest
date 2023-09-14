117d0fdf86d61a8fa70288542657d7cae628ec15
Fabius01k <pashamuruskinn@gmail.com>
2023-09-14 16:29:05 +0300

    versel.json

--- src/inputmodels-validation/blog.inputModel.ts
+++ src/inputmodels-validation/blog.inputModel.ts
@@ -1,14 +1,17 @@
 import { IsNotEmpty, IsString, Length, Matches } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class BlogInputModel {
   @IsString()
   @IsNotEmpty()
   @Length(1, 15)
+  @Transform(({ value }) => value.trim())
   name: string;
 
   @IsString()
   @IsNotEmpty()
   @Length(1, 500)
+  @Transform(({ value }) => value.trim())
   description: string;
   @IsString()
   @IsNotEmpty()
@@ -16,5 +19,6 @@ export class BlogInputModel {
   @Matches(
     /^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/,
   )
+  @Transform(({ value }) => value.trim())
   websiteUrl: string;
 }
