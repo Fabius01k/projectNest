050a5ca21e2028183e833ae0e65065225539d9f0
Fabius01k <pashamuruskinn@gmail.com>
2023-09-28 12:36:11 +0300

    versel.json

--- src/testingNest/testing.service.ts
+++ src/testingNest/testing.service.ts
@@ -1,35 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Blog } from '../blogNest/schema/blog-schema';
-import { Post } from '../postNest/schema/post-schema';
-import { InformationOfLikeAndDislikePost } from '../postNest/schema/likeOrDislikeInfoPost-schema';
-import { Comment } from '../commentNest/schema/comment.schema';
-import { InformationOfLikeAndDislikeComment } from '../commentNest/schema/likeOrDislikeInfoComment.schema';
-import { User } from '../userNest/schema/user.schema';
-import { UserSession } from '../userNest/schema/user-session.schema';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class TestingService {
   constructor(
-    @InjectModel(Blog.name) private blogModel: Model<Blog>,
-    @InjectModel(Post.name) private postModel: Model<Post>,
-    @InjectModel(InformationOfLikeAndDislikePost.name)
-    private PInfoModel: Model<InformationOfLikeAndDislikePost>,
-    @InjectModel(Comment.name) private commentModel: Model<Comment>,
-    @InjectModel(InformationOfLikeAndDislikeComment.name)
-    private CInfoModel: Model<InformationOfLikeAndDislikeComment>,
-    @InjectModel(User.name) private userModel: Model<User>,
-    @InjectModel(UserSession.name) private userSessionModel: Model<UserSession>,
+    @InjectDataSource()
+    protected dataSource: DataSource,
   ) {}
 
   async deleteAllData() {
-    await this.blogModel.deleteMany({});
-    await this.postModel.deleteMany({});
-    await this.PInfoModel.deleteMany({});
-    await this.commentModel.deleteMany({});
-    await this.CInfoModel.deleteMany({});
-    await this.userModel.deleteMany({});
-    await this.userSessionModel.deleteMany({});
+    await this.dataSource.query(`DELETE FROM public."Users"`);
+    await this.dataSource.query(`DELETE FROM public."UserSession"`);
   }
 }
