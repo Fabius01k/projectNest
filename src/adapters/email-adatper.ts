0476183818a38c4fd49f3c89f6b7aacb2ecbbd3f
Fabius01k <pashamuruskinn@gmail.com>
2023-09-30 14:34:52 +0300

    versel.json

--- src/adapters/email-adatper.ts
+++ src/adapters/email-adatper.ts
@@ -1,27 +1,26 @@
 import nodemailer from 'nodemailer';
 
-// export const emailAdapter = {
-//   async sendEmail(email: string, subject: string, message: string) {
-//     const transport = nodemailer.createTransport({
-//       host: 'smtp.yandex.ru',
-//       // port: 587,
-//       // secure: false,
-//       auth: {
-//         user: 'pav.murashckin@yandex.ru',
-//         pass: 'nhrdmesfbmhzfktd',
-//       },
-//     });
-//
-//     const info = await transport.sendMail({
-//       from: 'Pavel <pav.murashckin@yandex.ru>',
-//       to: email,
-//       subject: subject,
-//       html: message,
-//     });
-//     console.log('emailError2', info);
-//     return info;
-//   },
-// };
+export const emailAdapter = {
+  async sendEmail(email: string, subject: string, message: string) {
+    const transport = nodemailer.createTransport({
+      host: 'smtp.yandex.ru',
+      // port: 587,
+      // secure: false,
+      auth: {
+        user: 'pav.murashckin@yandex.ru',
+        pass: 'nhrdmesfbmhzfktd',
+      },
+    });
+
+    const info = await transport.sendMail({
+      from: 'Pavel <pav.murashckin@yandex.ru>',
+      to: email,
+      subject: subject,
+      html: message,
+    });
+    return info;
+  },
+};
 
 // const transport = nodemailer.createTransport({
 //   service: 'google',
@@ -34,26 +33,26 @@ import nodemailer from 'nodemailer';
 //   },
 // });
 
-export const emailAdapter = {
-  async sendEmail(email: string, subject: string, message: string) {
-    const transport = nodemailer.createTransport({
-      service: 'gmail',
-      // host: 'smtp.gmail.com',
-      // port: 465,
-      // secure: true,
-      auth: {
-        user: 'muraskinpavel5@gmail.com',
-        pass: 'dygyirzbaxsscjxw',
-      },
-    });
-
-    const info = await transport.sendMail({
-      from: 'Pavel <muraskinpavel5@gmail.com>',
-      to: email,
-      subject: subject,
-      html: message,
-    });
-
-    return info;
-  },
-};
+// export const emailAdapter = {
+//   async sendEmail(email: string, subject: string, message: string) {
+//     const transport = nodemailer.createTransport({
+//       service: 'gmail',
+//       // host: 'smtp.gmail.com',
+//       // port: 465,
+//       // secure: true,
+//       auth: {
+//         user: 'muraskinpavel5@gmail.com',
+//         pass: 'dygyirzbaxsscjxw',
+//       },
+//     });
+//
+//     const info = await transport.sendMail({
+//       from: 'Pavel <muraskinpavel5@gmail.com>',
+//       to: email,
+//       subject: subject,
+//       html: message,
+//     });
+//
+//     return info;
+//   },
+// };
