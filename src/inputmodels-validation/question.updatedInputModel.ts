40a7664a62c525c669dfec52e6768af6bcfd1151
Fabius01k <pashamuruskinn@gmail.com>
2023-11-05 16:50:55 +0300

    versel.json

--- src/inputmodels-validation/question.updatedInputModel.ts
+++ src/inputmodels-validation/question.updatedInputModel.ts
@@ -1,13 +1,13 @@
 import { IsBoolean, IsIn, IsNotEmpty, IsOptional } from 'class-validator';
 import { Transform, Type } from 'class-transformer';
 
-export class QuestionUpdatedInputModel {
-  @IsNotEmpty()
-  @IsBoolean()
-  published: boolean;
-}
 // export class QuestionUpdatedInputModel {
+//   @IsNotEmpty()
 //   @IsBoolean()
-//   @IsIn([true, false])
 //   published: boolean;
 // }
+export class QuestionUpdatedInputModel {
+  // @IsBoolean()
+  @IsIn([true, false])
+  published: boolean;
+}
